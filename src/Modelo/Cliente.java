60e650c573bd4fce0029f2a43d0d72446b475f3a
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2021-05-19 22:10:26 -0700

    Getters and Setters Cliente

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,55 @@
+package Modelo;
+
+public class Cliente {
+    int Id_cliente;
+    String Nombre_cliente;
+    String Telefono;
+    String Direccion;
+    
+   public Cliente()
+    {
+        
+    }
+   
+   public Cliente(int Id_cliente, String Nombre_cliente, String Telefono, String Direccion){
+       this.Id_cliente = Id_cliente;
+        this.Nombre_cliente = Nombre_cliente;
+        this.Telefono = Telefono;
+        this.Direccion = Direccion;
+        
+   }
+
+    public int getId_cliente() {
+        return Id_cliente;
+    }
+
+    public void setId_cliente(int Id_cliente) {
+        this.Id_cliente = Id_cliente;
+    }
+
+    public String getNombre_cliente() {
+        return Nombre_cliente;
+    }
+
+    public void setNombre_cliente(String Nombre_cliente) {
+        this.Nombre_cliente = Nombre_cliente;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+            
+    
+}
