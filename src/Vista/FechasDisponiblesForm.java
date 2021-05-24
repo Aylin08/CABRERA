6b266126568c0292be6c4c6a650a8611e349757b
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2021-05-24 09:33:32 -0700

    Calendario modificaciones

--- src/Vista/FechasDisponiblesForm.java
+++ src/Vista/FechasDisponiblesForm.java
@@ -14,13 +14,14 @@ public class FechasDisponiblesForm extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         btn_salir = new javax.swing.JButton();
+        rSCalendar1 = new rojeru_san.componentes.RSCalendar();
         ventana = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         btn_confirmar = new javax.swing.JButton();
         btn_salirCalendario = new javax.swing.JButton();
         jScrollPane4 = new javax.swing.JScrollPane();
         tabla_calendario = new javax.swing.JTable();
-        calendario = new rojeru_san.componentes.RSCalendar();
+        rSCalendar2 = new rojeru_san.componentes.RSCalendar();
 
         btn_salir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/cancelar.png"))); // NOI18N
         btn_salir.setToolTipText("Rechaza cotización");
@@ -79,41 +80,42 @@ public class FechasDisponiblesForm extends javax.swing.JInternalFrame {
         });
         jScrollPane4.setViewportView(tabla_calendario);
 
-        calendario.setColorBackground(new java.awt.Color(255, 153, 51));
-        calendario.setColorButtonHover(new java.awt.Color(255, 255, 51));
-        calendario.setColorDiaActual(new java.awt.Color(255, 102, 102));
+        rSCalendar2.setColorBackground(new java.awt.Color(255, 102, 0));
+        rSCalendar2.setColorButtonHover(new java.awt.Color(255, 153, 51));
+        rSCalendar2.setColorDiaActual(new java.awt.Color(255, 51, 0));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane4)
-                .addContainerGap())
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(12, Short.MAX_VALUE)
-                .addComponent(calendario, javax.swing.GroupLayout.PREFERRED_SIZE, 425, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(21, 21, 21)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btn_salirCalendario, javax.swing.GroupLayout.DEFAULT_SIZE, 78, Short.MAX_VALUE)
-                    .addComponent(btn_confirmar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(21, 21, 21))
+                .addComponent(rSCalendar2, javax.swing.GroupLayout.PREFERRED_SIZE, 476, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btn_confirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_salirCalendario, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(250, 250, 250))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(calendario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(136, 136, 136)
+                        .addGap(157, 157, 157)
                         .addComponent(btn_salirCalendario)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(btn_confirmar)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+                        .addComponent(btn_confirmar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(rSCalendar2, javax.swing.GroupLayout.DEFAULT_SIZE, 341, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)))
+                .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -121,13 +123,11 @@ public class FechasDisponiblesForm extends javax.swing.JInternalFrame {
         ventana.setLayout(ventanaLayout);
         ventanaLayout.setHorizontalGroup(
             ventanaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 686, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         ventanaLayout.setVerticalGroup(
             ventanaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(ventanaLayout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -167,9 +167,10 @@ public class FechasDisponiblesForm extends javax.swing.JInternalFrame {
     private javax.swing.JButton btn_confirmar;
     private javax.swing.JButton btn_salir;
     private javax.swing.JButton btn_salirCalendario;
-    private rojeru_san.componentes.RSCalendar calendario;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane4;
+    private rojeru_san.componentes.RSCalendar rSCalendar1;
+    private rojeru_san.componentes.RSCalendar rSCalendar2;
     private javax.swing.JTable tabla_calendario;
     private javax.swing.JPanel ventana;
     // End of variables declaration//GEN-END:variables
