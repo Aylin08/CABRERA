cad4875e35f3c5a3e3785054e2023f7c8cf14d89
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2021-05-26 09:54:49 -0700

    Generar Serie

---
+++ src/Modelo/Pedidos.java
@@ -0,0 +1,39 @@
+
+package Modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author AYLIN
+ */
+public class Pedidos {
+    Connection con;
+    Conexion cn= new Conexion();
+    PreparedStatement ps;
+    ResultSet rs;
+    
+   public String NoSerieVentas()
+  {
+      String serie="";
+      String sql ="Select max(Numero_ventas) from ventas";
+      try{
+        con=cn.Conector();
+        ps=con.prepareStatement(sql);
+        rs=ps.executeQuery();
+        while(rs.next())
+        {
+              serie=rs.getString(1);  
+        }
+      }
+      catch(Exception e){
+          
+      }
+      return serie;
+  }
+    
+    
+    
+}
